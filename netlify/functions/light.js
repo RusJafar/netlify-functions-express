a30233cd08190498d19abe35de7d2a7887ca3278
rzamkovoy <r.zamkovoy@cfa.digital>
2024-02-29 14:16:19 +0300

    Add light preset path

--- netlify/functions/light.js
+++ netlify/functions/light.js
@@ -1,6 +1,5 @@
 exports.handler = async (event, context, callback) => {
-
-
+    //test
     if(event.httpMethod === 'GET' && event.path === '/preset/951/on') {
         await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light");
         return {
