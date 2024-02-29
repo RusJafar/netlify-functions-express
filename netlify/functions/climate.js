dbb0e715368e726840811c7c3ad0bf39fb3c5c63
rzamkovoy <r.zamkovoy@cfa.digital>
2024-02-29 14:29:41 +0300

    added paths /preset/951/on - off

--- netlify/functions/climate.js
+++ netlify/functions/climate.js
@@ -1,14 +1,31 @@
-exports.handler = async (event, context) => {
-    await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=climate");
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            data: null,
-            error: null
-        }),
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'Access-Control-Allow-Origin': '*',
+exports.handler = async (event, context, callback) => {
+    if (event.httpMethod === 'GET' && event.path === '/agent/light/preset/951/on') {
+        await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=climate/preset/951/on");
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                data: null,
+                error: null
+            }),
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Access-Control-Allow-Origin': '*',
+            }
         }
     }
+    if (event.httpMethod === 'GET' && event.path === '/agent/light/preset/951/on') {
+        await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=climate/preset/951/off");
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                data: null,
+                error: null
+            }),
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+    }
+
 }
--- netlify/functions/light.js
+++ netlify/functions/light.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context, callback) => {
-    if(event.httpMethod === 'GET' && event.path === '/preset/951/on') {
-        await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=/preset/951/on");
+    if (event.httpMethod === 'GET' && event.path === '/preset/951/on') {
+        await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light/preset/951/on");
         return {
             statusCode: 200,
             body: JSON.stringify({
@@ -12,8 +12,9 @@ exports.handler = async (event, context, callback) => {
                 'Access-Control-Allow-Origin': '*',
             }
         }
-    } if(event.httpMethod === 'GET' && event.path === '/preset/951/off')  {
-        await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=/preset/951/off");
+    }
+    if (event.httpMethod === 'GET' && event.path === '/preset/951/off') {
+        await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light/preset/951/off");
         return {
             statusCode: 200,
             body: JSON.stringify({
