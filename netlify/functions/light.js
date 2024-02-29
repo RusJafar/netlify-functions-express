8725f8b72e6b9f2b43ff1ce36d5f524fb8e6e005
rzamkovoy <r.zamkovoy@cfa.digital>
2024-02-29 14:13:59 +0300

    Add light preset path

--- netlify/functions/light.js
+++ netlify/functions/light.js
@@ -1,15 +1,32 @@
-exports.handler = async (event, context) => {
-    await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light");
+exports.handler = async (event, context, callback) => {
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            data: null,
-            error: null
-        }),
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            'Access-Control-Allow-Origin': '*',
+
+    if(event.httpMethod === 'GET' && event.path === '/preset/951/on') {
+        await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light");
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                data: null,
+                error: null
+            }),
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+    } else {
+        await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light");
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                data: null,
+                error: null
+            }),
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Access-Control-Allow-Origin': '*',
+            }
         }
     }
+
 }
