8ae3828e48379cac3fdce06b83f42abee3b9db1b
rzamkovoy <r.zamkovoy@cfa.digital>
2024-02-29 14:30:41 +0300

    added paths /preset/951/on - off

--- netlify/functions/light.js
+++ netlify/functions/light.js
@@ -1,5 +1,5 @@
 exports.handler = async (event, context, callback) => {
-    if (event.httpMethod === 'GET' && event.path === '/preset/951/on') {
+    if (event.httpMethod === 'GET' && event.path === '/agent/light/preset/951/on') {
         await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light/preset/951/on");
         return {
             statusCode: 200,
@@ -13,7 +13,7 @@ exports.handler = async (event, context, callback) => {
             }
         }
     }
-    if (event.httpMethod === 'GET' && event.path === '/preset/951/off') {
+    if (event.httpMethod === 'GET' && event.path === '/agent/light/preset/951/on') {
         await fetch("https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light/preset/951/off");
         return {
             statusCode: 200,
