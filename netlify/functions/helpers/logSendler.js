5852605834c1e486b5bf481cdf5ef581b22fdcd7
rzamkovoy <r.zamkovoy@cfa.digital>
2024-03-01 15:04:08 +0300

    added paths /preset/951/on - off

---
+++ netlify/functions/helpers/logSendler.js
@@ -0,0 +1,5 @@
+const logSendler = async (message) => {
+    await fetch(`https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=${message}`);
+}
+
+export default logSendler;
--- netlify/functions/light.js
+++ netlify/functions/light.js
@@ -1,7 +1,8 @@
+const logSendler = require("./helpers/logSendler");
 exports.handler = async (event, context, callback) => {
-    if (event.httpMethod === 'POST' && event.path === '/agent/light/preset/951/on') {
+    if (event.httpMethod === 'POST' && event.path.match(/\/agent\/light\/preset\/(.*?)\/on/)) {
+        await logSendler(event.path);
         const level = event.queryStringParameters['level'] && null;
-        await fetch(`https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light/preset/951/on${level? `level=${level}`: ''}`);
         return {
             statusCode: 200,
             body: JSON.stringify({
@@ -14,8 +15,8 @@ exports.handler = async (event, context, callback) => {
             }
         }
     }
-    if (event.httpMethod === 'POST' && event.path === '/agent/light/preset/951/off') {
-        await fetch('https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=light/preset/951/off');
+    if (event.httpMethod === 'POST' && event.path.match(/\/agent\/light\/preset\/(.*?)\/off/)) {
+        await logSendler(event.path);
         return {
             statusCode: 200,
             body: JSON.stringify({
