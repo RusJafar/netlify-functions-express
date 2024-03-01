4ca2e99a38d9c6daa2fbed3cd7984891cc83b861
rzamkovoy <r.zamkovoy@cfa.digital>
2024-03-01 16:43:03 +0300

    added paths /preset/951/on - off

--- netlify/functions/agent.js
+++ netlify/functions/agent.js
@@ -5,7 +5,7 @@ const logSendler = async (message) => {
 
 exports.handler = async (event, context, callback) => {
     if (event.httpMethod === 'POST') {
-        const level = event.queryStringParameters? event.queryStringParameters['level']: null;
+        const level = event.queryStringParameters['level'];
         await logSendler(event.path + `${level? `?level=${JSON.stringify(level)}`: ''}`);
         // await fetch(`https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=${event.path}`);
         return {
