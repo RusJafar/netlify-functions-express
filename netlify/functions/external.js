9f40d63db026562cab13d8870a15f9e87797c8c4
rzamkovoy <r.zamkovoy@cfa.digital>
2024-04-15 15:23:23 +0300

    added external function for Work booth mock

---
+++ netlify/functions/external.js
@@ -0,0 +1,37 @@
+const logSendler = async (message) => {
+    const msg = `WorkBooth: ${message}`;
+    await fetch(`https://api.telegram.org/bot5787873665:AAE2duy-dHpJjzJFv3EPYi2n6aE2CkmnS5s/sendMessage?chat_id=-1001816393133&text=${msg}`);
+}
+
+exports.handler = async (event, context, callback) => {
+    if(event.path.includes()) {
+
+    }
+    if (event.httpMethod === 'POST') {
+        const level = event.queryStringParameters['level'];
+        await logSendler(event.path + `request path ${event.path}`);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                data: null,
+                error: null
+            }),
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+    }
+
+    return {
+        statusCode: 404,
+        body: JSON.stringify({
+            data: null,
+            error: "Path not exist"
+        }),
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Access-Control-Allow-Origin': '*',
+        }
+    }
+}
